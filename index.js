fe9d165eb2de47a1a3a75a88d11556d8b09e7d90
DantorKing <hd8afdez@hotmail.com>
2021-11-24 14:55:54 -0600

    Cambia las coordenadas del mapa para AGS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 let api_key="AIzaSyABYfdNAPibREcz-3Clw0sKIQlkW6QzgLg";
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 21.9338292, lng: -102.3196637 },
     zoom: 8,
   });
 }
